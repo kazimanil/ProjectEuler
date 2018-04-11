a1080f1e4d2b23803f96a500c36290da2cd719e3
Kazım Anıl Eren <anil.eren@digitallency.com>
2018-04-11 12:04:06 +0300

    Revamped Repo Structure with a more sleek design.

--- Euler#7.R
+++
@@ -1,8 +0,0 @@
-j <- 2
-while (i < 10001){
-  for(h in 1:j){
-    j %% h == 0
-  }
-  j <- j + 1
-  print(j)
-}
--- Incomplete Answers/Euler #35.R
+++ Incomplete Answers/Euler #35.R
@@ -2,4 +2,3 @@ source("Prime Number Detector.R")
 # this function derives a list of prime numbers upto specified number.
 primenumbers <- as.data.table(pnd(1000000))
 fwrite(primenumbers, "primenumbers.csv")
-# NON COMPLETE
